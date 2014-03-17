e9852cb06e6b95e33a89e702d243022013d821be
David Meads <david.meads@cengage.com>
2014-03-16 20:05:45 -0400

    Fix pathing issue.

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ var glob = require('glob'),
 
 module.exports = function(pattern, options) {
     return glob.sync(pattern, options).map(function(file) {
-        return require(process.cwd() + '/' + cleanFile(file));
+        return require(cleanFile(file, options));
     });
 };
 
-function cleanFile(file) {
-    return path.normalize(file).replace(process.cwd(), '');
+function cleanFile(file, options) {
+    var cwd = options.cwd || process.cwd();
+    return path.join(cwd, path.normalize(file).replace(cwd, ''));
 }
